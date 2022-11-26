96e59febf3b40c369d22937c6f87b63249fcadc7
Morgan <mdezen@udel.edu>
2022-11-26 12:42:04 -0500

    break into components

--- src/components/Suggestion.tsx
+++ src/components/Suggestion.tsx
@@ -1,28 +1,58 @@
 import { useState, ChangeEvent } from "react";
 
 function Suggestion() {
+    // use state changes suggestion ='' to suggestion= event.target.value and re-renders
+    // the component 
     const [suggestion, setSuggestion] = useState<string>('');
+    const [name, setName] = useState<string>('');
 
+    // this is called when the button is clicked 
+    // takes in the invoke url which inlcudes the resource 
+    // invoke URL is where the API gateway is located
+    // fetch makes an http request to that url and then returns the http response
     async function postSuggestion() {
         await fetch("https://0wbaztinr8.execute-api.us-east-1.amazonaws.com/prod/suggestion", {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ suggestion })
+            // sets the body of the request to srtrings in JSON form (key/value?) 
+            body: JSON.stringify({ suggestion, name })
         });
     };
 
+    
+    // when the input text changes, it runs suggestionInputChange which 
+    // takes in event with type ChangeEvent..... and passes event.target.value 
+    // (the actual text) to setSuggestion
     function suggestionInputChange(event: ChangeEvent<HTMLInputElement>) {
         setSuggestion(event.target.value);
     }
+
+    function nameInputChange(event: ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value);
+    }
+
     return (
         <div>
             <form>
+            <label>
+                Name:
                 <input
-                    type="text"
-                    id="SuggestionText"
-                    placeholder="Type your suggestion"
-                    onChange={suggestionInputChange}
-                />
+                        type="text"
+                        id="NameText"
+                        placeholder="Type your Name"
+                        onChange={nameInputChange}
+                    />
+            </label>
+                <br></br>
+                <label>
+                    Suggestion:
+                    <input
+                        type="text"
+                        id="SuggestionText"
+                        placeholder="Type your suggestion"
+                        onChange={suggestionInputChange}
+                    />
+                </label>
             </form>
             <button onClick={() => postSuggestion()}>
                 Submit
@@ -32,3 +62,5 @@ function Suggestion() {
 }
 
 export default Suggestion
+
+
--- src/components/Vote.tsx
+++ src/components/Vote.tsx
@@ -1,32 +1,33 @@
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
+
 
 function Vote() {
-    type resultProps = {
-        yesCount: number;
-        noCount: number;
-    };
-    const [result, setResult] = useState<resultProps>();
+    // these initialize 2 stateful variables result and vote and says what type they are
     const [vote, setVote] = useState<string>();
+    const [currentYesCount, setCurrentYesCount] = useState<number>();
+    const [currentNoCount, setCurrentNoCount] = useState<number>();
 
+    // this func is called when the input fields change (y/n radio buttons)
+    // it takes in the change event and sets the vote variable to the value (y or n)
     const voteInputChange = (event:
-        React.ChangeEvent<HTMLInputElement>) => {
+        ChangeEvent<HTMLInputElement>) => {
         setVote(event.target.value);
     };
 
     async function postVote() {
+// fetch makes an http request to that url and returns the response
+// it sets data variable to that response which inlcudse the y/n vote counts
         const data = await fetch("https://0wbaztinr8.execute-api.us-east-1.amazonaws.com/prod/vote", {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ vote })
         });
+
+// .json converts it to JSON form (a dictionary)
+// yesCount and noCount just pull the yes /no count out of the jsonData dict
         const jsonData = await data.json();
-        const yesCount = jsonData.body.yesCount;
-        const noCount = jsonData.body.noCount;
-        console.log(yesCount)
-        setResult({
-            yesCount: yesCount,
-            noCount: noCount
-        });
+        setCurrentYesCount(jsonData.body.yesCount);
+        setCurrentNoCount(jsonData.body.noCount);
     };
 
     return (
@@ -57,17 +58,35 @@ function Vote() {
                     Submit
                 </button>
             </fieldset>
-
-            <h2>Results</h2>
-            <p>
-                Coors is Water: {result?.yesCount} votes
-            </p>
-            <p>
-                Coors is NOT Water: {result?.noCount} votes
-            </p>
+            { currentYesCount 
+                ? <VoteResults
+                    yesCount={currentYesCount}
+                    noCount={currentNoCount}
+                /> 
+                : null
+            }
         </>
 
     )
 }
-
 export default Vote
+
+// resultProps is a type we made up. It is a dictionary? with 2 key value pairs
+type VoteResultProps = {
+    yesCount: number | undefined;
+    noCount: number | undefined;
+};
+function VoteResults ({yesCount, noCount}: VoteResultProps) { 
+    return (
+        <>
+            <h2>Results</h2>
+            <p>
+                Coors is Water: {yesCount} votes
+            </p>
+            <p>
+                Coors is NOT Water: {noCount} votes
+            </p>
+        </>
+    )
+}
+
